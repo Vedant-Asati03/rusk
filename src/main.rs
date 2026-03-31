dbc59e10187799b078e16b7ffec0ca9f9f7cec9b
vedant-asati03 <vedant.asati03@gmail.com>
2026-04-01 01:58:28 +0530

    Refactor main function to return io::Result and organize module imports

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    
+mod buffer;
+mod editor;
+mod terminal;
+mod view;
+
+use editor::Editor;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut editor = Editor::default()?;
+    editor.run()?;
+    Ok(())
 }
