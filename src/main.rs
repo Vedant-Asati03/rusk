fda452eb60ad8b31fa5543dbc15c432ffa3c2eb5
vedant-asati03 <vedant.asati03@gmail.com>
2026-04-05 02:36:04 +0530

    handling filename if given otherwise default behaviour

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@ mod terminal;
 mod view;
 
 use editor::Editor;
+use std::env;
 use std::io;
 
 fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    let filename = match args.get(1) {
+        Some(name) => name,
+        None => "",
+    };
+
     let mut editor = Editor::default()?;
-    editor.run()?;
+    editor.run(filename)?;
     Ok(())
 }
