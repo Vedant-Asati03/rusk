9682b678a0d756e62e7511aa6ec2b00c217d833c
vedant-asati03 <vedant.asati03@gmail.com>
2026-04-05 12:50:07 +0530

    Simplify filename handling in main function and update editor initialization

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,9 @@ use std::env;
 use std::io;
 
 fn main() -> io::Result<()> {
-    let args: Vec<String> = env::args().collect();
+    let filename = env::args().nth(1).unwrap_or_default();
 
-    let filename = match args.get(1) {
-        Some(name) => name,
-        None => "",
-    };
-
-    let mut editor = Editor::default()?;
-    editor.run(filename)?;
+    let mut editor = Editor::new()?;
+    editor.run(&filename)?;
     Ok(())
 }
